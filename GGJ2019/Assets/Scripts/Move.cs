82a25917f6f19f0f4068769c64959c39e4f18713
Unknown <giuseppe.ellena@studenti.unimi.it>
2019-01-26 11:00:01 +0100

    ADD script for moving

---
+++ GGJ2019/Assets/Scripts/Move.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Move : MonoBehaviour {
+
+    // Use this for initialization
+    public float speedX = 10f;
+    public float speedY = 10f;
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float y=CrossPlatformInputManager.GetAxis("Vertical")*speedX;
+        float x = CrossPlatformInputManager.GetAxis("Horizontal") * speedY;
+        x *= Time.deltaTime;
+        y *= Time.deltaTime;
+        transform.Translate(x, y, 0);
+       
+	}
+}
---
+++ GGJ2019/Assets/Scripts/NetworkServerUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking.NetworkSystem;
+
+public class NetworkServerUI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        UnityEngine.Networking.NetworkServer.Listen(2500);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        string ipAddress = Network.player.ipAddress;
+        GUI.Box(new Rect(10,Screen.))
+	}
+}
